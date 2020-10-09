5cdbe105483e9382b758bc4f7159b5b7e5aeacdd
Scott <scott.r.ja@gmail.com>
2020-10-09 13:52:09 -0500

    Parishes are now being showed on activity 1 Image added yo activty one as well. Button needs to be added to actvity

--- app/src/main/java/com/uwi/loanhub/MainActivity.kt
+++ app/src/main/java/com/uwi/loanhub/MainActivity.kt
@@ -22,13 +22,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        /* For city drop down on the main activity*/
         var cityTowns:TextInputLayout = findViewById<TextInputLayout>(R.id.city_TextInputLayout)
         var  cities =resources.getStringArray(R.array.cities) //Drop Down Items
         var autoCompleteCityText = findViewById<AutoCompleteTextView>(R.id.drop_text_city)
         var aa = ArrayAdapter(this, R.layout.dropdown_city_towns, cities)
         aa.setDropDownViewResource(android.R.layout.simple_list_item_1)
         autoCompleteCityText!!.setAdapter(aa)
+
+        /* For city drop down on the main activity*/
+
+        /* For Parish drop down on the main activity*/
+        var parishLayout:TextInputLayout = findViewById<TextInputLayout>(R.id.parish_TextInputLayout)
+        var parishes =resources.getStringArray(R.array.parishes) //Drop Down Items
+        var autoCompleteParishText = findViewById<AutoCompleteTextView>(R.id.drop_text_parish)
+        var parishesOut = ArrayAdapter(this, R.layout.dropdown_parish, parishes)
+        parishesOut.setDropDownViewResource(android.R.layout.simple_list_item_1)
+        autoCompleteParishText!!.setAdapter(parishesOut)
+
+        /* For parish drop down on the main activity*/
 
 
 
