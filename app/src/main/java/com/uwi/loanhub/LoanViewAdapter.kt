215158282593561595eb56a5aecb506b73ca23c3
Scott <scott.r.ja@gmail.com>
2020-10-12 22:39:18 -0500

    Formatting done to getView

--- app/src/main/java/com/uwi/loanhub/LoanViewAdapter.kt
+++ app/src/main/java/com/uwi/loanhub/LoanViewAdapter.kt
@@ -1,13 +1,15 @@
 package com.uwi.loanhub
 
 import android.content.Context
+import android.icu.text.DecimalFormat
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import org.w3c.dom.Text
+import androidx.annotation.RequiresApi
 
 class LoanViewAdapter(loanListModel: LoanListViewModel, c_context: Context): BaseAdapter() {
 
@@ -19,6 +21,13 @@ class LoanViewAdapter(loanListModel: LoanListViewModel, c_context: Context): Bas
         loansModel = loanListModel
         context = c_context
 
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun currencyFormatter(num: String): String? {
+        val m = num.toDouble()
+        val formatter = DecimalFormat("###,###,###")
+        return formatter.format(m)
     }
 
     override fun getCount(): Int {
@@ -36,14 +45,15 @@ class LoanViewAdapter(loanListModel: LoanListViewModel, c_context: Context): Bas
         return loansModel.loan[position].hashCode().toLong()
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val layoutInflator = LayoutInflater.from(context)
         val row = layoutInflator.inflate(R.layout.layoutlistviewloan, parent, false)
         row.findViewById<ImageView>(R.id.institutionImage).setImageResource(loansModel.loan[position].institution.getLogo())
         row.findViewById<TextView>(R.id.institutionName).text = loansModel.loan[position].institution.getinstituionName()
         row.findViewById<TextView>(R.id.loanName).text = loansModel.loan[position].loanName
-        row.findViewById<TextView>(R.id.loanAmount).text = loansModel.loan[position].loanAmount.toString()
-        row.findViewById<TextView>(R.id.loanInterestRate).text = loansModel.loan[position].interestRate.toString()
+        row.findViewById<TextView>(R.id.loanAmount).text = "Amount: $".plus(currencyFormatter(loansModel.loan[position].loanAmount.toString()))
+        row.findViewById<TextView>(R.id.loanInterestRate).text =  "Interest Rate: ".plus(loansModel.loan[position].interestRate.toString()).plus("%")
 
         return row
 
