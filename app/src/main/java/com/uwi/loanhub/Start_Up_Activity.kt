dbfa75b18fe8e26f9c74ddab940bf7aa652ae8cf
Scott <scott.r.ja@gmail.com>
2020-10-16 00:58:56 -0500

    design of the first screen created

--- app/src/main/java/com/uwi/loanhub/Start_Up_Activity.kt
+++ app/src/main/java/com/uwi/loanhub/Start_Up_Activity.kt
@@ -2,10 +2,12 @@ package com.uwi.loanhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Start_Up_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start__up_)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
