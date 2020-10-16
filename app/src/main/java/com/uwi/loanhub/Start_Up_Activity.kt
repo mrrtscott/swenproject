aaf6137deb1e042d65319ea1cead821b945bd63d
Scott <scott.r.ja@gmail.com>
2020-10-15 23:12:17 -0500

    Ninety per cent of the activity start up page has completed

---
+++ app/src/main/java/com/uwi/loanhub/Start_Up_Activity.kt
@@ -0,0 +1,11 @@
+package com.uwi.loanhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Start_Up_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start__up_)
+    }
+}
