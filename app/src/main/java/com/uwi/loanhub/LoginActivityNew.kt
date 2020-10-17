1d8976ff6690ba71f44837919780b886e7c7ad49
Scott <scott.r.ja@gmail.com>
2020-10-16 19:38:57 -0500

    graphics for activity login are now in place

---
+++ app/src/main/java/com/uwi/loanhub/LoginActivityNew.kt
@@ -0,0 +1,11 @@
+package com.uwi.loanhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivityNew : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_new)
+    }
+}
