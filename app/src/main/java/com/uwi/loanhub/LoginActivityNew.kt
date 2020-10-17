ee1fba349abad4ebb65cddb4e2336e1d7b2896ad
Scott <scott.r.ja@gmail.com>
2020-10-16 20:05:02 -0500

    graphics for activity login are now in place 1.0

--- app/src/main/java/com/uwi/loanhub/LoginActivityNew.kt
+++ app/src/main/java/com/uwi/loanhub/LoginActivityNew.kt
@@ -2,10 +2,12 @@ package com.uwi.loanhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivityNew : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_new)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
--- app/src/main/java/com/uwi/loanhub/Start_Up_Activity.kt
+++ app/src/main/java/com/uwi/loanhub/Start_Up_Activity.kt
@@ -1,13 +1,28 @@
 package com.uwi.loanhub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class Start_Up_Activity : AppCompatActivity() {
+
+    private lateinit var startButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start__up_)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
+        startButton = findViewById(R.id.startActivityButton)
+
+        startButton.setOnClickListener {
+            val intent: Intent = Intent(this, LoginActivityNew::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
