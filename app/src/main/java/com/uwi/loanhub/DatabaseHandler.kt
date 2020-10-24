5856434ceb0ded11fd883709f38c4cdab2ec20f6
Scott <scott.r.ja@gmail.com>
2020-10-23 22:11:23 -0500

    loan table constant created

--- app/src/main/java/com/uwi/loanhub/DatabaseHandler.kt
+++ app/src/main/java/com/uwi/loanhub/DatabaseHandler.kt
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHandler (context: Context): SQLiteOpenHelper  (context, DATABASE_NAME, null, DATABASE_VERSION){
+
+
     companion object{
 
         private const val DATABASE_VERSION = 1
@@ -44,6 +46,20 @@ class DatabaseHandler (context: Context): SQLiteOpenHelper  (context, DATABASE_N
         private const val CREATE_TABLE_USER = "CREATE TABLE" + TABLE_USERS + "(" + KEY_ID + " INTEGER PRIMARY KEY, " + USER_FIRST_NAME + " TEXT, " + USER_LAST_NAME + " TEXT, " + USER_EMAIL + " TEXT, " + USER_USERNAME + " TEXT, " + USER_PASSWORD + " TEXT, " + USER_SEX + " TEXT," + USER_DOB + " TEXT," + USER_SALARY + " TEXT," + USER_CITY + " TEXT," + USER_PARISH + " TEXT," + USER_PRIMARY_BANK + " TEXT," + USER_LOAN_TYPE + " TEXT," + USER_LOAN_AMOUNT + " REAL," + USER_OCCUPATION + " TEXT," + KEY_CREATED_AT + " DATETIME" + ")"
 
 
+        //LOAN TABLES
+
+        private const val LOAN_INSTITUTION = "institution"
+        private const val LOAN_NAME = "loanName"
+        private const val LOAN_AMOUNT = "loanAmount"
+        private const val LOAN_INTEREST_RATE = "loanInterestRate"
+        private const val LOAN_TERMS_REPAY = "loanTermsRepay"
+        private const val LOAN_PERCENT_FINANCING = "loanPercentFinancing"
+        private const val LOAN_CREDIT_SCORE = "percentCreditScore"
+        private const val LOAN_DESCRIPTION = "loanDescription"
+        private const val LOAN_STATUS = "loanStatus"
+
+        private const val CREATE_TABLE_LOANS = "CREATE TABLE" + TABLE_LOANS + "(" + KEY_ID + " INTEGER PRIMARY KEY, " + LOAN_INSTITUTION + " TEXT, " + LOAN_NAME + " TEXT, " + LOAN_AMOUNT + " REAL, " + LOAN_INTEREST_RATE + " REAL, " + LOAN_TERMS_REPAY + " INTEGER, " + LOAN_PERCENT_FINANCING + " REAL," + LOAN_CREDIT_SCORE + " INTEGER," + LOAN_DESCRIPTION + " TEXT," + LOAN_STATUS + " TEXT," + KEY_CREATED_AT + " DATETIME" + ")"
+
 
 
 
@@ -53,8 +69,14 @@ class DatabaseHandler (context: Context): SQLiteOpenHelper  (context, DATABASE_N
 
     }
 
-    override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+    override fun onCreate(db: SQLiteDatabase?) {
+
+        if (db != null) {
+            db.execSQL(CREATE_TABLE_USER)
+            db.execSQL(CREATE_TABLE_LOANS)
+
+        }
+
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
--- app/src/main/java/com/uwi/loanhub/models/LoanModel.kt
+++ app/src/main/java/com/uwi/loanhub/models/LoanModel.kt
@@ -1,4 +1,4 @@
 package com.uwi.loanhub.models
 
 
-class LoanModel(val id: String, val institution : String, val loanName: String, val loanAmount: Int, val interestRate: Double, val termsRepay:String, val percentFinancing: Double, val creditScore: Int, val description: String, val status: String )
+class LoanModel(val id: Int, val institution : String, val loanName: String, val loanAmount: Int, val interestRate: Double, val termsRepay:String, val percentFinancing: Double, val creditScore: Int, val description: String, val target:ArrayList<String>, val status: String )
