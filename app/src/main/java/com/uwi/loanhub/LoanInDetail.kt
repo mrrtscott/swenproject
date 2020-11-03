5fc2ad5305eec914686503462dbec8ed467e8efb
Scott <scott.r.ja@gmail.com>
2020-11-03 00:54:43 -0500

    Id added to loan detail logo

--- app/src/main/java/com/uwi/loanhub/LoanInDetail.kt
+++ app/src/main/java/com/uwi/loanhub/LoanInDetail.kt
@@ -3,6 +3,8 @@ package com.uwi.loanhub
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.uwi.loanhub.models.LoanInstitutionViewModel
@@ -30,6 +32,22 @@ class LoanInDetail : AppCompatActivity() {
         loanInstitutionViewModel.specificLoanDetail.observe(this, Observer { loans ->
             println(loans.size)
             println(loans.get(0).loanName)
+
+
+            val institutionName = findViewById<TextView>(R.id.institutionNameLoanDetails)
+            val institutionLogo = findViewById<ImageView>(R.id.institutionLogoLoanDetails)
+            val loanName = findViewById<TextView>(R.id.loanNameLoanDetails)
+            val loanEmail = findViewById<TextView>(R.id.institutionEmailLoanDetails)
+            val loanPhone = findViewById<TextView>(R.id.institutionPhoneLoanDetails)
+            val loanDetails = findViewById<TextView>(R.id.loanDescriptionLoanDetails)
+            val loanBranches= findViewById<TextView>(R.id.loanBranchLoanDetails)
+            val loanAmount = findViewById<TextView>(R.id.loanAmountLoanDetails)
+            val loanInterestRate = findViewById<TextView>(R.id.loanInterestRateLoanDetails)
+            val loanRepay = findViewById<TextView>(R.id.loanTermsRepayLoanDetails)
+            val loanPerCentFinance= findViewById<TextView>(R.id.loanPerCentFinancingLoanDetails)
+            val loanMinCreditScore = findViewById<TextView>(R.id.loanMinCreditScoreLoanDetails)
+
+
         })
 
 
