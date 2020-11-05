337ef3913795bba0892bc0a33994d0bf47ea9daa
Scott <scott.r.ja@gmail.com>
2020-11-05 12:35:16 -0500

    Insert function added to loan requirement

--- app/src/main/java/com/uwi/loanhub/models/LoanRequirementDao.kt
+++ app/src/main/java/com/uwi/loanhub/models/LoanRequirementDao.kt
@@ -1,6 +1,8 @@
 package com.uwi.loanhub.models
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
@@ -8,4 +10,8 @@ interface LoanRequirementDao {
 
     @Query("SELECT * FROM LoanRequirement WHERE LoanRequirement.loanID = :inputLoanID AND LoanRequirement.institutionName = :inputInstitutionName")
     fun getAllRequirements(inputLoanID:Int, inputInstitutionName:String)
+
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addLoanRequirement(requirement: LoanRequirement)
 }
