e9230dafabdcc53ee6ca63974c4e175056833f3a
Scott <scott.r.ja@gmail.com>
2020-11-25 04:34:53 -0500

    Old Institution class Code clean up completed

--- app/src/main/java/com/uwi/loanhub/Institution.kt
+++ app/src/main/java/com/uwi/loanhub/Institution.kt
@@ -2,6 +2,14 @@
 
 package com.uwi.loanhub
 
+/**
+ * An old class which was used to create a banking institution
+ * @param inputInstitution An input which would represent the name of the institution
+ * @param inputEmail An input which would represent the email address associated with the institution
+ * @param inputPhone An input which would represent the phone number associated with the institution
+ * @param inputBranch An input which would contain the list of names of branches associated with the bank of type ArrayList
+ * @param inputLogo An input which would have the logo ID number of branch that is stored in the resources module
+ */
 class Institution(inputInstitution:String, inputEmail: String, inputPhone: String, inputBranch: Array<String>, inputLogo:Int ) {
 
     private var institutionName:String = inputInstitution
@@ -11,42 +19,42 @@ class Institution(inputInstitution:String, inputEmail: String, inputPhone: Strin
     private var logo: Int = inputLogo
 
 
-    /*
-    A public function which is responsible for returning the ID of a logo to be used for resource purposes
-    @return Int: logoID
+    /**
+    * A public function which is responsible for returning the ID of a logo to be used for resource purposes
+    * @return A logoID from the resources file
     */
     fun getLogo (): Int {
         return logo
     }
 
-    /*
-    A public function which returns the name an institution
-    @return String: Institution Name
-     */
+    /**
+    * A public function which returns the name an institution
+    * @return String: Institution Name
+     * */
     fun getInstitutionName (): String {
         return institutionName
     }
 
-    /*
-    A public function which returns the email address of an institution
-    @return String: Institution Email Address
+    /**
+    *A public function which returns the email address of an institution
+    * @return String: Institution Email Address
      */
     fun getEmail(): String {
         return email
     }
 
-    /*
-    A public function which returns the phone number an institution as a string
-    @return String: Institution Phone
+    /**
+    *A public function which returns the phone number an institution as a string
+    * @return String: Institution Phone
      */
     fun getPhone (): String {
         return phone
     }
 
 
-    /*
-    A public function which returns the branches of an institution as an array
-    @return Array: Institution Branches
+    /**
+    * A public function which returns the branches of an institution as an array
+    * @return Array: Institution Branches
      */
     fun getBranch (): Array<String>{
         return branch
