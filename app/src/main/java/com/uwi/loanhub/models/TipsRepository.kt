c43525603d59ddebf829f6b9bd706550831b792c
Scott <scott.r.ja@gmail.com>
2020-11-25 17:50:49 -0500

    UPDATED: Tips cleaned and completed

--- app/src/main/java/com/uwi/loanhub/models/TipsRepository.kt
+++ app/src/main/java/com/uwi/loanhub/models/TipsRepository.kt
@@ -2,6 +2,10 @@ package com.uwi.loanhub.models
 
 import androidx.lifecycle.LiveData
 
+
+/**
+ * The tips repository class which manages the data for the tips
+ */
 class TipsRepository(private val inputTipsDao: TipsDao) {
 
     val allTips:LiveData<List<Tips>> = inputTipsDao.getTips()
