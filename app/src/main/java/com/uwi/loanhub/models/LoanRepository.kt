17c4b7723df52d59ae87a61520f9c5a041adbd0c
Scott <scott.r.ja@gmail.com>
2020-11-25 17:40:43 -0500

    Loan repository completed

--- app/src/main/java/com/uwi/loanhub/models/LoanRepository.kt
+++ app/src/main/java/com/uwi/loanhub/models/LoanRepository.kt
@@ -2,6 +2,10 @@ package com.uwi.loanhub.models
 
 
 import androidx.lifecycle.LiveData
+
+/**
+ * The repository class which manages the data for each loan
+ */
 class LoanRepository(private val inputLoanDao: LoanDao) {
 
     val allLoans: LiveData<List<Loan>> = inputLoanDao.getAllLoans()
